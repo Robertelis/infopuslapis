3904a7702657d4b37331683f129bb5e105f5916e
Robertelis <csgoeaglecom@gmail.com>
2016-02-27 21:50:22 +0200

    test
    This reverts commit c6fcf270532cce3e241dd0ae04125b842209a46f.

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Valgau vaikus</title>
-    </head>
-        <body background-color: red>
-            <h1 style="font-family: Oxygen; color:red; font-size:20px">vaikas</h1>
-        </body>
-</html>
